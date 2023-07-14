0beb16f78856579f814f54681c2d92f56b0a211b
wilson perez <wilpec2017@gmail.com>
2023-07-13 21:07:59 -0500

    Esqueleto 10 Enero

---
+++ funciones.py
@@ -0,0 +1,17 @@
+# git init
+# git list
+# git git config --global user.name "wilson perez"
+# git config --global user.email "wilpec2017@gmail.com"
+# git config --global --unset-all user.email "wilpec2045@gmail.com"
+#  git config --lits
+import os
+os.system("cls")
+
+def funcion1 ():
+    pass
+def funcion2 ():
+    pass
+def funcion3 ():
+    pass
+def funcion4 ():
+    pass
